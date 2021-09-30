fba1582e280519d50bbb12f259eab2bdea913e36
tonyboy <anton.sandu68@mail.ru>
2021-09-30 02:05:49 +0200

    Ajout dynamique des liens dans la navbar + amélioration présentation

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,29 +4,45 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('prenom', TextType::class, [
+                'label' => '请输您的名字 - Ajouter votre prénom'
+            ])
+            ->add('nom', TextType::class, [
+                'label' => '请输您的姓 - Ajouter votre nom'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => '请输您的电话号码 - Ajouter un numéro de téléphone'
+            ])
+            ->add('email', EmailType::class,[
+                'label'=> '请输一个电子邮箱 - Ajouter un email',
+                'attr' => [
+                    'placeholder' => '请输您的电子邮箱 - Saisir ici votre email',]
+            ])
+            /*->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ])
+            ])*/
             ->add('plainPassword', PasswordType::class, [
+                'label'=> '请输您的密码 - Ajouter un mot de passe',
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
